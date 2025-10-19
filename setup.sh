9f0160f78045a4ef07171c0321d378a311720aaf
Spicy-Steve <crazygamerdude26@gmail.com>
2025-10-19 17:17:30 +1300

    Update 2

--- setup.sh
+++ setup.sh
@@ -48,12 +48,10 @@ if [ -f /etc/arch-release ]; then
     if ! command -v yay &>/dev/null; then
         echo "Installing yay (AUR helper)..."
         sudo pacman -S --needed --noconfirm base-devel git
-        tmpdir=$(mktemp -d)
-        git clone https://aur.archlinux.org/yay.git "$tmpdir"
-        cd "$tmpdir"
+        mkdir ~/.yay
+        git clone https://aur.archlinux.org/yay-bin.git ~/.yay
+        cd ~/.yay
         makepkg -si --noconfirm
-        cd -
-        rm -rf "$tmpdir"
     else
         echo "yay is already installed."
     fi
@@ -102,6 +100,11 @@ else
     echo "Oh My Zsh already installed."
 fi
 
+# === Install extra zsh stuff ===
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 # === Copy Zsh config ===
 if [ -f "$DOTFILES_DIR/.zshrc" ]; then
     echo "Applying .zshrc from dotfiles..."
@@ -114,7 +117,6 @@ if [ -d "$DOTFILES_DIR/oh-my-zsh-custom" ]; then
     mkdir -p "$HOME/.oh-my-zsh/custom/"
     cp -r "$DOTFILES_DIR/oh-my-zsh-custom/"* "$HOME/.oh-my-zsh/custom/"
 fi
-
 
 # === KDE Appearance ===
 echo "Applying KDE customization..."
@@ -183,12 +185,6 @@ if [ -d "$DOTFILES_DIR/konsole" ]; then
     cp -r "$DOTFILES_DIR/konsole/"* ~/.local/share/konsole/
 fi
 
-# Restart Plasma shell to apply changes (optional)
-if pgrep plasmashell &>/dev/null; then
-    echo "Restarting Plasma shell..."
-    pkill plasmashell && kstart5 plasmashell &
-fi
-
 # === Change default shell to zsh ===
 if [ "$SHELL" != "$(which zsh)" ]; then
     echo "Changing default shell to zsh..."
@@ -196,8 +192,8 @@ if [ "$SHELL" != "$(which zsh)" ]; then
 fi
 
 # === Package Installation ===
-read -p "Would you like to install packages? [Y/n]" appsq
-if [ ${appsq,,} = "y" ] || [ ${appsq,,} = "yes" ] || [ $appsq = "" ]; then
+read -p "Would you like to install packages? [Y/n]" {appsq,,}
+if [ $appsq = "y" ] || [ $appsq = "yes" ] || [ -z $appsq ]; then
     if [ -f /etc/fedora-release ]; then
         $PKG_INSTALL android-tools ark btop cava cmatrix discord easyeffects ffmpeg-full fastfetch flatpak goverlay mangohud pavucontrol prismlauncher python python-websockets qbittorrent qt6-qtwebsockets-devel rar speedtest-cli steam vlc vlc-plugins-all
     elif [ -f /etc/arch-release ]; then
@@ -209,16 +205,16 @@ else
 fi
 
 # === Flatpak Installation ===
-read -p "Would you like to install flatpak && flatpak apps? [Y/n]" flatpakq
-if [ ${flatpakq,,} = "y" ] || [ ${flatpakq,,} = "yes" ] || [ $flatpakq = "" ]; then
+read -p "Would you like to install flatpak && flatpak apps? [Y/n]" {flatpakq,,}
+if [ $flatpakq = "y" ] || [ $flatpakq = "yes" ] || [ -z $flatpakq ]; then
     flatpak install com.dec05eba.gpu_screen_recorder com.github.Matoking.protontricks com.github.tchx84.Flatseal com.steamgriddb.SGDBoop com.vysp3r.ProtonPlus io.missioncenter.MissionCenter it.mijorus.gearlever org.localsend.localsend_app
 else
     echo "Skipping flatpak installation..."
 fi
 
 # === Package Installation (AUR) ===
-read -p "Would you like to install AUR packages? [Y/n]" aur_appsq
-if [ ${aur_appsq,,} = "y" ] || [ ${aur_appsq,,} = "yes" ] || [ $aur_appsq = "" ]; then
+read -p "Would you like to install AUR packages? [Y/n]" {aur_appsq,,}
+if [ $aur_appsq = "y" ] || [ $aur_appsq = "yes" ] || [ -z $aur_appsq ]; then
     yay visual-studio-code-bin coolercontrol coolercontrold plasma6-applets-kurve
 else
     echo "Skipping AUR package installation..."
@@ -232,6 +228,12 @@ if [ -f /etc/arch-release ]; then
     else
         echo "No pacman.conf found in dotfiles, skipping..."
     fi
+fi
+
+# Restart Plasma shell to apply changes (optional)
+if pgrep plasmashell &>/dev/null; then
+    echo "Restarting Plasma shell..."
+    pkill plasmashell && kstart5 plasmashell &
 fi
 
 fastfetch
